c0f8c8f8694ce8251f150e35cd972f5a83ad2379
Navyamathan <navyamathan@gmail.com>
2025-05-13 15:42:47 +0530

    Time: 19 ms (12.20%) | Memory: 50.4 MB (5.71%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,21 +1,48 @@
 class Solution {
     public boolean isPalindrome(String s) {
+
         s = s.replaceAll("[^A-Za-z0-9]" , "").toLowerCase();
-        int index = s.length();
-        String rev = "";
-        String result = calculation(index,rev,s);
-        if(s.equals(result))
+        System.out.println(s);
+        if(s.isEmpty())
         {
             return true;
         }
-        return false;
+        int i = 0;
+        boolean result = calculation(i , s);
+        return result;
     }
-    public String calculation(int i,String rev,String s)
+
+    public boolean calculation(int i,String s)
     {
-        if(i <= 0)
+        if(i >= s.length()/2)
         {
-            return rev;
+            return true;
         }
-        return calculation(i-1,rev+s.charAt(i-1),s);
+        if(s.charAt(i) != s.charAt(s.length()-1-i))
+        {
+            return false;
+        }
+        return calculation(i+1,s);
     }
+
+
+
+    //     s = s.replaceAll("[^A-Za-z0-9]" , "").toLowerCase();
+    //     int index = s.length();
+    //     String rev = "";
+    //     String result = calculation(index,rev,s);
+    //     if(s.equals(result))
+    //     {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    // public String calculation(int i,String rev,String s)
+    // {
+    //     if(i <= 0)
+    //     {
+    //         return rev;
+    //     }
+    //     return calculation(i-1,rev+s.charAt(i-1),s);
+    // }
 }
