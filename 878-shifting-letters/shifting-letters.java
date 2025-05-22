12bbe6297ca534e76bebf0a39da5a2a483e7a5c4
Navyamathan <navyamathan@gmail.com>
2025-05-23 00:11:07 +0530

    Time: 6 ms (90.50%) | Memory: 57.1 MB (70.63%) - LeetSync

--- 878-shifting-letters/shifting-letters.java
+++ 878-shifting-letters/shifting-letters.java
@@ -4,11 +4,23 @@ class Solution
     {
        char[] ch = s.toCharArray();
        int sum = 0;
-       for(int i=ch.length-1; i>=0; i--)
+       for(int i=shifts.length-1; i>=0; i--)
        {
-        sum = (sum + shifts[i])%26;
-        ch[i] = (char) ('a' + (sum + ch[i] - 'a') % 26);
+        sum = (sum + shifts[i]) % 26;
+        ch[i] = (char) ('a' + (ch[i] - 'a' + sum) % 26);
        }
        return new String(ch);
     }
 }
+
+
+
+
+//   long maxofi = 0;
+//         char[] chars = s.toCharArray();
+//         for (int i = shifts.length - 1; i >= 0; i--) {
+//             maxofi += shifts[i];
+//             chars[i] = (char) 
+//             ('a' + (chars[i] - 'a' + maxofi) % 26);
+//         }
+//         return new String(chars);
