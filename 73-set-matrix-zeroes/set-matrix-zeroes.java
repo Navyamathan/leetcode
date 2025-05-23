1dd56bf84c7ed288c75e83e3297fa783998eb20d
Navyamathan <navyamathan@gmail.com>
2025-05-23 11:21:09 +0530

    Time: 1 ms (73.30%) | Memory: 45.7 MB (64.14%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -5,30 +5,28 @@ class Solution
         int n = matrix.length;
         int m = matrix[0].length;
 
-        int col0 = 1;
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[m];
+
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
             {
                 if(matrix[i][j] == 0)
                 {
-                    matrix[i][0] = 0;
-
-                    if(j != 0)
-                    matrix[0][j] = 0;
-                    else
-                    col0 = 0;
+                   row[i] = true;
+                   col[j] = true;
                 }
             }
         }
 
-        for(int i=1; i<n; i++)
+        for(int i=0; i<n; i++)
         {
-            for(int j=1; j<m; j++)
+            for(int j=0; j<m; j++)
             {
                 if(matrix[i][j] != 0)
                 {
-                    if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    if(row[i] || col[j])
                     {
                         matrix[i][j] = 0;
                     }
@@ -36,19 +34,5 @@ class Solution
             }
         }
 
-        if(matrix[0][0] == 0)
-        {
-            for(int j=0; j<m; j++)
-            {
-                matrix[0][j] = 0;
-            }
-        }
-        if(col0 == 0)
-        {
-            for(int i=0; i<n; i++)
-            {
-                matrix[i][0] = 0;
-            }
-        }
     }
 }
