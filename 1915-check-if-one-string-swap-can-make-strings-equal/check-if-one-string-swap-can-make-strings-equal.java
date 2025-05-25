405b92515ef0813d71c4def8e20ed770753b5a18
Navyamathan <navyamathan@gmail.com>
2025-05-25 21:38:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (39.40%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -8,6 +8,7 @@ class Solution
         }
         int change1 = -1;
         int change2 = -1;
+        int count = 0;
         for(int i=0; i<s1.length(); i++)
         {
             if(s1.charAt(i) != s2.charAt(i))
@@ -20,17 +21,16 @@ class Solution
                 {
                     change1 = i;
                 }
+                count++;
             }
         }
-        if(change1 != -1 && change2 != -1)
+        if(count > 2 || count < 2)
         {
-            String temp = s2.substring(0,change1)+s2.charAt(change2);
-            temp = temp+s2.substring(change1+1,change2)+s2.charAt(change1)+s2.substring(change2+1);
-            System.out.println(temp);
-            if(s1.equals(temp))
-            {
-                return true;
-            }
+            return false;
+        }
+        if(s1.charAt(change1) == s2.charAt(change2) && s1.charAt(change2) == s2.charAt(change1))
+        {
+            return true;
         }
         return false;
     }
