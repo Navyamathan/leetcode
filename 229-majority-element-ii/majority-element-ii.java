c3eb95d7dfccf9571e70534ecf6a3271caa70969
Navyamathan <navyamathan@gmail.com>
2025-05-27 10:44:23 +0530

    Time: 3 ms (74.33%) | Memory: 50.4 MB (20.85%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -2,38 +2,60 @@ class Solution
 {
     public List<Integer> majorityElement(int[] nums) 
     {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        ArrayList<Integer> list = new ArrayList<>();
-        int length = nums.length/3 + 1;
+        int count1 = 0, count2 = 0;
+        int element1 = Integer.MIN_VALUE, element2 = Integer.MIN_VALUE;
         for(int i=0; i<nums.length; i++)
         {
-            if(!map.containsKey(nums[i]))
+            if(count1 == 0 && nums[i] != element2)
             {
-                map.put(nums[i],1);
-                int count = map.get(nums[i]);
-                if(count == length)
-                {
-                    list.add(nums[i]);
-                }
-                else if(count <= length+1)
-                {
-                    continue;
-                }
+                element1 = nums[i];
+                count1++;
+            }
+            else if(count2 == 0 && nums[i] != element1)
+            {
+                element2 = nums[i];
+                count2 = 1;
+            }
+            else if(nums[i] == element1)
+            {
+                count1++;
+            }
+            else if(nums[i] == element2)
+            {
+                count2++;
             }
             else
             {
-                map.put(nums[i], map.get(nums[i])+1);
-                int count = map.get(nums[i]);
-                if(count == length)
-                {
-                    list.add(nums[i]);
-                }
-                else if(count <= length+1)
-                {
-                    continue;
-                }
+                count1--;
+                count2--;
             }
         }
+
+        count1 = 0;
+        count2 = 0;
+        for(int i=0; i<nums.length; i++)
+        {
+            if(nums[i] == element1)
+            {
+                count1++;
+            }
+            else if(nums[i] == element2)
+            {
+                count2++;
+            }
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int max = nums.length/3;
+        if(count1 > max )
+        {
+            list.add(element1);
+        }
+        if(count2 > max)
+        {
+            list.add(element2);
+        }
+
         return list;
     }
 }
