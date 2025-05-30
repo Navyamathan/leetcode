b14bdeb716c2c77eddc38895535ee40455f1b6c6
Navyamathan <navyamathan@gmail.com>
2025-05-30 10:00:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (88.95%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -2,49 +2,68 @@ class Solution
 {
     public void merge(int[] nums1, int m, int[] nums2, int n) 
     {
-        int len = m+n;
-        int gap = (len/2)+(len%2);
-        while(gap > 0)
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(j >= 0)
         {
-            int left = 0;
-            int right = left+gap;
-            while(right < len)
+            if(i>=0 && nums1[i] > nums2[j])
             {
-                if(left < m && right >= m)
-                {
-                    swap(nums1,left, nums2, right-m);
-                }
-                else if(left >= m)
-                {
-                    swap(nums2, left-m, nums2, right-m);
-                }
-                else
-                {
-                    swap(nums1, left, nums1, right);
-                }
-                left++;
-                right++;
+                nums1[k] = nums1[i];
+                k--;
+                i--;
             }
-            if(gap == 1)
+            else
             {
-                break;
+                nums1[k] = nums2[j];
+                k--;
+                j--;
             }
-            gap = (gap/2)+(gap%2);
-        }
-
-        for(int i=m,j=0; i<nums1.length; i++,j++)
-        {
-            nums1[i] = nums2[j];
         }
     }
+    //     int len = m+n;
+    //     int gap = (len/2)+(len%2);
+    //     while(gap > 0)
+    //     {
+    //         int left = 0;
+    //         int right = left+gap;
+    //         while(right < len)
+    //         {
+    //             if(left < m && right >= m)
+    //             {
+    //                 swap(nums1,left, nums2, right-m);
+    //             }
+    //             else if(left >= m)
+    //             {
+    //                 swap(nums2, left-m, nums2, right-m);
+    //             }
+    //             else
+    //             {
+    //                 swap(nums1, left, nums1, right);
+    //             }
+    //             left++;
+    //             right++;
+    //         }
+    //         if(gap == 1)
+    //         {
+    //             break;
+    //         }
+    //         gap = (gap/2)+(gap%2);
+    //     }
 
-    public void  swap(int[] arr1, int i1, int[] arr2, int i2)
-    {
-        if(arr1[i1] >= arr2[i2] )
-        {
-            int temp = arr1[i1];
-            arr1[i1] = arr2[i2];
-            arr2[i2] = temp;
-        }
-    }
+    //     for(int i=m,j=0; i<nums1.length; i++,j++)
+    //     {
+    //         nums1[i] = nums2[j];
+    //     }
+    // }
+
+    // public void  swap(int[] arr1, int i1, int[] arr2, int i2)
+    // {
+    //     if(arr1[i1] >= arr2[i2] )
+    //     {
+    //         int temp = arr1[i1];
+    //         arr1[i1] = arr2[i2];
+    //         arr2[i2] = temp;
+    //     }
+    // }
 }
