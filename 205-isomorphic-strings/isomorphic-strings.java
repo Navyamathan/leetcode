7f33ef6036a6dc56fa4848e5ba94cce63b5bcc7e
Navyamathan <navyamathan@gmail.com>
2025-06-12 15:23:41 +0530

    Time: 5 ms (97.98%) | Memory: 42.4 MB (71.05%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -2,59 +2,23 @@ class Solution
 {
     public boolean isIsomorphic(String s, String t) 
     {
-         if(s.length() != t.length())
+        if(s.length() != t.length())
         {
             return false;
         }
-        HashMap<Character,ArrayList<Integer>> h1 = new HashMap<>();
-        HashMap<Character,ArrayList<Integer>> h2 = new HashMap<>();
+        int[] arrayS = new int[128];
+        int[] arrayT = new int[128];
 
-        String s1 = "";
-        int i = 0;
-        for(char ch : s.toCharArray())
+        for(int i=0; i<s.length(); i++)
         {
-            if(h1.containsKey(ch))
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            if(arrayS[ch1] != arrayT[ch2])
             {
-                h1.get(ch).add(i);
+                return false;
             }
-            else
-            {
-                s1 += ch;
-                h1.put(ch, new ArrayList<>());
-                h1.get(ch).add(i);
-            }
-            i++;
-        }
-        String s2 = "";
-        int j = 0;
-        for(char ch : t.toCharArray())
-        {
-            if(h2.containsKey(ch))
-            {
-                h2.get(ch).add(j);
-            }
-            else
-            {
-                s2 += ch;
-                h2.put(ch, new ArrayList<>());
-                h2.get(ch).add(j);
-            }
-            j++;
-        }
-
-        if(s1.length() != s2.length())
-        {
-            return false;
-        }
-
-        for(int k=0; k<s1.length(); k++)
-        {
-           char c1 = s1.charAt(k);
-           char c2 = s2.charAt(k);
-           if(!h1.get(c1).equals(h2.get(c2)))
-           {
-            return false;
-           }
+            arrayS[ch1] = i+1;
+            arrayT[ch2] = i+1;
         }
         return true;
     }
