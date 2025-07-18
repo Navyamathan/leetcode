7fa5c52881f4821a0fa0498c5ff1d57ada970e95
Navyamathan <navyamathan@gmail.com>
2025-07-18 22:21:47 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (98.01%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.java
+++ 203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -12,27 +12,38 @@ class Solution
 {
     public ListNode removeElements(ListNode head, int v) 
     {
+        // ---> 3rd way (recursion (take all elements as head and iterate))
+
         if(head == null) return null;
-        
-        while(head != null && head.val == v)
-        {
-            head = head.next;
-        }
+        head.next = removeElements(head.next,v);
+        return head.val == v ? head.next : head;
 
-        ListNode current = head;
-        while(current != null && current.next != null)
-        {
-            if(current.next.val == v)
-            {
-                current.next = current.next.next;
-            }
-            else
-            {
-                current = current.next;
-            }
-        }
 
-        return head; 
+
+        // ---> 2nd way (2 while 1->head 2->elemnts)
+
+        // if(head == null) return null;
+        // while(head != null && head.val == v)
+        // {
+        //     head = head.next;
+        // }
+        // ListNode current = head;
+        // while(current != null && current.next != null)
+        // {
+        //     if(current.next.val == v)
+        //     {
+        //         current.next = current.next.next;
+        //     }
+        //     else
+        //     {
+        //         current = current.next;
+        //     }
+        // }
+        // return head; 
+
+
+
+        // ---> 1st way (use dummy 0 index)
 
         // ListNode dummy = new ListNode(0,head);
         // ListNode current = dummy;
